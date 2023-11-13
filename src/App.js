8438619d644d14ceb2418ad75cc8e7384eb6abd5
Harikesh2 <mths83478@gmail.com>
2023-11-13 20:07:34 +0530

    working the album collections!!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Album from "./component/Album";
 
 class App extends React.Component {
   render(){
     return (
     <>
     <h1>Album Collection</h1>
+    <Album />
     </>
       );
 
